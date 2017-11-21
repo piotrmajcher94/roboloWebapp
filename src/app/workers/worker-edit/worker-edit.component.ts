0868359eb07acad8f8a5c4071b040ba1267dad1b
Olaf Krawczyk <radeko94@gmail.com>
2017-11-21 12:25:42 +0100

    create project form

--- src/app/workers/worker-edit/worker-edit.component.ts
+++ src/app/workers/worker-edit/worker-edit.component.ts
@@ -1,4 +1,6 @@
+import { FormGroup, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms/src/model';
 
 @Component({
   selector: 'app-worker-edit',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkerEditComponent implements OnInit {
 
+  workerForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.workerForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'surname': new FormControl(null, Validators.required),
+    });
   }
-
 }
